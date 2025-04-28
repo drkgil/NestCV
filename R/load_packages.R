b120bd2c601e7b3af8723d72b5e810afe2da4634
Karli Gilbert <karli.gilbert@gwu.edu>
2025-04-28 13:49:28 -0400

    loadpkg fix 2

--- R/load_packages.R
+++ R/load_packages.R
@@ -1,25 +1,38 @@
-#' @title Load required packages
-#' @description Loads packages listed in the `cran_packages` object.
-#' @export
-
 load_packages <- function() {
-  # List of CRAN packages to install and load
   cran_packages <- c("knitr", "kableExtra", "dplyr", "ggplot2", "tidyr", "janitor",
                      "ggalluvial", "xgboost", "rBayesianOptimization", "Amelia",
                      "patchwork", "SHAPforxgboost", "tidyquant", "tidyverse",
                      "caret", "PRROC", "viridis", "randomForest", "JOUSBoost",
                      "rpart", "naivebayes", "psych", "ggsignif", "pROC", "gridExtra",
-                     "glmnet", "pheatmap", "ppcor", "reshape2", "shapviz", "kernelshap",
+                     "glmnet", "pheatmap", "ppcor", "reshape2", "kernelshap",
                      "RColorBrewer")
 
-  # Loop over the package list
+  # For custom packages not on CRAN (e.g., shapviz)
+  github_packages <- c("shapviz")
+
+  # Install CRAN packages
   for (pkg in cran_packages) {
-    # If the package is not installed, install it
     if (!requireNamespace(pkg, quietly = TRUE)) {
       install.packages(pkg)
     }
+    # Try loading the package
+    tryCatch({
+      library(pkg, character.only = TRUE)
+    }, error = function(e) {
+      message(paste("Package", pkg, "could not be loaded:", e$message))
+    })
+  }
 
-    # Try loading the package and catch any errors
+  # Install GitHub packages
+  for (pkg in github_packages) {
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      tryCatch({
+        devtools::install_github(pkg)
+      }, error = function(e) {
+        message(paste("GitHub package", pkg, "could not be installed:", e$message))
+      })
+    }
+    # Try loading the package
     tryCatch({
       library(pkg, character.only = TRUE)
     }, error = function(e) {
@@ -27,4 +40,3 @@ load_packages <- function() {
     })
   }
 }
-
